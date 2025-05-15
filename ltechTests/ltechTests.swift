b6ec31cb1458d695d8b50ba86a3496eb544a91ba
Leonid Perlin <perlinleo@gmai.com>
2025-05-15 16:40:40 +0300

    initial commit

---
+++ ltechTests/ltechTests.swift
@@ -0,0 +1,17 @@
+//
+//  ltechTests.swift
+//  ltechTests
+//
+//  Created by blacksnow on 5/15/25.
+//
+
+import Testing
+@testable import ltech
+
+struct ltechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
